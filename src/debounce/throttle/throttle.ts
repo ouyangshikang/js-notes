9760f0e500c2337668bf6e10c6e1b1bbc49e2285
baituo <baituo@gaoding.com>
2022-10-23 23:32:50 +0800

    feat: throttle

--- src/debounce/throttle/throttle.ts
+++ src/debounce/throttle/throttle.ts
@@ -1,16 +1,13 @@
 /**
- * 防抖函数
+ * 节流函数
  * @param fn 执行的回调函数
  * @param delay 延迟时间
  */
-function debounce(fn: () => void, delay: number = 200) {
+function throttle(fn: () => void, delay: number = 100) {
     let timer: any = null;
 
-    // this 是个假的参数，它出现在参数列表的最前面(参考 ts 中 this 的使用)
     return function (this: void, ...args: any) {
-        if (timer) {
-            clearTimeout(timer);
-        }
+        if (timer) return;
 
         timer = setTimeout(() => {
             fn.apply(this, args);
